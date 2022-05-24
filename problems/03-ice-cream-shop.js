6591e0c724cd4bb3da08ceced9aad80758bdbbfb
Matthew Hutter <matthewjhutter@gmail.com>
2022-05-24 11:08:41 -0700

    03 solved

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -17,17 +17,12 @@ iceCreamShop([], 'honey lavender'); // false
 function iceCreamShop(flavors, favorite) {
   if (favorite === false) return false;
   if (flavors.length === 0) return false;
-  let flavor = flavors[0];
+  let flavor = flavors.shift();
   if (flavor === favorite) return true;
-  
-
+  return (iceCreamShop(flavors, favorite));
 }
 
-console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
-console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
-console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
-console.log(iceCreamShop(['moose tracks'], 'moose tracks')); // true
-console.log(iceCreamShop([], 'honey lavender')); // false
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = iceCreamShop;
