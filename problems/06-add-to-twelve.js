81cc88e67a661361341d8c1f4e735ea3f836455b
Justin <hangjustin@gmail.com>
2022-05-24 13:47:20 -0700

    06 solved

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -12,7 +12,17 @@ addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
 
-// your code here
+let addToTwelve = (array) => {
+
+  if (array.length === 1) return false;
+
+  if (array[0] + array[1] === 12) return true;
+
+  array.shift()
+
+  return addToTwelve(array);
+
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
