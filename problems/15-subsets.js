8bc7e171205d5fe81ce75b9368ce75761b81f70c
Justin <hangjustin@gmail.com>
2022-05-25 14:42:37 -0700

    15 revisit this

--- problems/15-subsets.js
+++ problems/15-subsets.js
@@ -15,25 +15,90 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
 ***********************************************************************/
 
 // your code here
+
 const subsets = (arr) => {
-  if (arr.length === 0) return [];
 
-  let subArr = [];
-  let firstEl = arr[0];
-  if (subArr === undefined) return subArr.push([]);
-  if (arr.length === 1) return subArr.push(firstEl);
+  if (!arr.length) return [[]];
+  debugger
+  let prevSubsets = subsets(arr.slice(0, arr.length - 1));    // arr = [1, 2]    sliced => []
+  // console.log(`prevSubsets: ${prevSubsets}`)
+  debugger
+  let last = arr[arr.length - 1];                     // last = 2
+  // console.log(last)
+  debugger
+  // console.log('This is prevSubet :', prevSubsets)
+  let addedSubsets = prevSubsets.map(subArray => {
+    debugger
+    return [...subArray, last]
+  })
+  // [ [] ]             [  [1]  ]
+  // console.log(addedSubsets)
 
-  //look at array indiceies, determine if we want them there or not, go to next index, etc.
-  let currArr = arr.slice(0, 1);
-
-  
-  // if (arr.length === 2) {
-
-  // }
+  debugger
+  return prevSubsets.concat(addedSubsets);
 
 }
-subsets([1]) // [[], [1]]
-console.log(subsets([1]));
+subsets([1, 2, 3])
+// const subsets = (arr) => {
+//   if (!arr.length) return [[]];
+
+//   const last = arr[arr.length - 1];
+//   const subs = subsets(arr.slice(0, arr.length - 1));
+
+//   return subs.concat(subs.map((el) => {
+//     let newArr = el.slice(0)
+//     newArr.push(last);
+//     return newArr;
+//   }));
+// }
+
+
+
+
+// const subsets = (arr, subArr = []) => {
+//   if (arr.length === 0) return [[]];
+//   // let subArr = [];
+//   // let subArr = [];
+//   // let firstEl = arr[0];
+//   // if (subArr === undefined) return subArr.push([]);
+//   // if (arr.length === 1) return subArr.push(firstEl);
+
+
+//   //look at array indiceies, determine if we want them there or not, go to next index, etc.
+//   let tempPop = arr.slice(0, 1);
+//   let tempInc = arr.slice(0, 1);
+
+//   debugger
+//   // Don't include
+//   // console.log(tempPop)
+//   // console.log(tempInc)
+//   // console.log(subArr);
+//   tempPop.pop();
+//   // subArr.push(arr.slice(0, 0));
+//   // subArr.push(tempPop)
+//   debugger
+//   subArr.push(tempPop.concat(subsets(arr.slice(1)), subArr))
+//   debugger;
+//   // subArr.push(tempPop);
+
+//   // debugger;
+//   // Include this element
+//   // subArr.push(tempInc);
+//   debugger;
+//   subArr.push(tempInc.concat(subsets(arr.slice(1)), subArr))
+//   debugger;
+//   // subArr.push(tempInc);
+//   // debugger;
+
+//   console.log(subArr);
+//   // if (arr.length === 2) {
+
+//   // }
+
+//   // return subArr;
+// }
+// subsets([1, 2]) // [[], [1]]
+// console.log(subsets([1, 2, 3]));
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = subsets;
